4507d46d624dcd0cc544290bd7f9f66466a68454
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 20:26:02 +0630

    Format Code

--- DotNet8WebApi.EFCoreInfiniteScrollPagination.Api/Features/Blog/BL_Blog.cs
+++ DotNet8WebApi.EFCoreInfiniteScrollPagination.Api/Features/Blog/BL_Blog.cs
@@ -1,46 +1,48 @@
-﻿using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features.Blog;
-using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features;
-using DotNet8WebApi.EFCoreInfiniteScrollPagination.Repositories.Features.Blog;
-using Microsoft.AspNetCore.Http.HttpResults;
+﻿using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features;
+using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features.Blog;
 using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Resources;
+using DotNet8WebApi.EFCoreInfiniteScrollPagination.Repositories.Features.Blog;
 
-namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Api.Features.Blog
+namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Api.Features.Blog;
+
+public class BL_Blog
 {
-    public class BL_Blog
+    private readonly IBlogRepository _blogRepository;
+
+    public BL_Blog(IBlogRepository blogRepository)
     {
-        private readonly IBlogRepository _blogRepository;
+        _blogRepository = blogRepository;
+    }
 
-        public BL_Blog(IBlogRepository blogRepository)
+    public async Task<Result<BlogListResponseModel>> GetBlogs(int id, int pageSize)
+    {
+        Result<BlogListResponseModel> responseModel;
+        try
         {
-            _blogRepository = blogRepository;
-        }
-
-        public async Task<Result<BlogListResponseModel>> GetBlogs(int id, int pageSize)
-        {
-            Result<BlogListResponseModel> responseModel;
-            try
+            if (id <= 0)
             {
-                if (id <= 0)
-                {
-                    responseModel = Result<BlogListResponseModel>.FailureResult(MessageResource.InvalidId);
-                    goto result;
-                }
-
-                if (pageSize <= 0)
-                {
-                    responseModel = Result<BlogListResponseModel>.FailureResult(MessageResource.InvalidPageSize);
-                    goto result;
-                }
-
-                responseModel = await _blogRepository.GetBlogs(id, pageSize);
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<BlogListResponseModel>.FailureResult(ex);
+                responseModel = Result<BlogListResponseModel>.FailureResult(
+                    MessageResource.InvalidId
+                );
+                goto result;
             }
 
-        result:
-            return responseModel;
+            if (pageSize <= 0)
+            {
+                responseModel = Result<BlogListResponseModel>.FailureResult(
+                    MessageResource.InvalidPageSize
+                );
+                goto result;
+            }
+
+            responseModel = await _blogRepository.GetBlogs(id, pageSize);
         }
+        catch (Exception ex)
+        {
+            responseModel = Result<BlogListResponseModel>.FailureResult(ex);
+        }
+
+    result:
+        return responseModel;
     }
 }
