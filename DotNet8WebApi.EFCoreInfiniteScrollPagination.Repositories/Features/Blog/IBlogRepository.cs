a120d263060e697edf10794b2f456e686208e140
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 20:31:19 +0630

    Format Code

--- DotNet8WebApi.EFCoreInfiniteScrollPagination.Repositories/Features/Blog/IBlogRepository.cs
+++ DotNet8WebApi.EFCoreInfiniteScrollPagination.Repositories/Features/Blog/IBlogRepository.cs
@@ -1,15 +1,9 @@
 ﻿using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features;
 using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features.Blog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Repositories.Features.Blog
+namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Repositories.Features.Blog;
+
+public interface IBlogRepository
 {
-    public interface IBlogRepository
-    {
-        Task<Result<BlogListResponseModel>> GetBlogs(int id, int pageSize);
-    }
+    Task<Result<BlogListResponseModel>> GetBlogs(int id, int pageSize);
 }
