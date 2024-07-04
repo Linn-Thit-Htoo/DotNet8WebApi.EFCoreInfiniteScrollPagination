ca0f135f94e917e44ab07cdcc1d24e6d79e778e8
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 20:29:50 +0630

    Format Code

--- DotNet8WebApi.EFCoreInfiniteScrollPagination.Models/Features/Blog/BlogListResponseModel.cs
+++ DotNet8WebApi.EFCoreInfiniteScrollPagination.Models/Features/Blog/BlogListResponseModel.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features.Blog;
 
-namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features.Blog
+public class BlogListResponseModel
 {
-    public class BlogListResponseModel
-    {
-        public List<BlogModel> DataLst { get; set; }
+    public List<BlogModel> DataLst { get; set; }
 
-        public BlogListResponseModel(List<BlogModel> dataLst)
-        {
-            DataLst = dataLst;
-        }
+    public BlogListResponseModel(List<BlogModel> dataLst)
+    {
+        DataLst = dataLst;
     }
 }
