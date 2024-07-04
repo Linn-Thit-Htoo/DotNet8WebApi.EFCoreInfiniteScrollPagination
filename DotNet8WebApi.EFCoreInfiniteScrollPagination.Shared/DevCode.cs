98d0a5befdc075629cccb411e3c616fab35e9417
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 20:32:23 +0630

    Format Code

--- DotNet8WebApi.EFCoreInfiniteScrollPagination.Shared/DevCode.cs
+++ DotNet8WebApi.EFCoreInfiniteScrollPagination.Shared/DevCode.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Shared;
 
-namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Shared
+public class DevCode
 {
-    public class DevCode
-    {
-    }
 }
