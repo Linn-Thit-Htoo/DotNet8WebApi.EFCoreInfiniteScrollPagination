2c1724a66cec4d474d7d770a977417a798a43afa
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 20:26:58 +0630

    Format Code

--- DotNet8WebApi.EFCoreInfiniteScrollPagination.Api/Features/BaseController.cs
+++ DotNet8WebApi.EFCoreInfiniteScrollPagination.Api/Features/BaseController.cs
@@ -1,20 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+﻿namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Api.Features;
 
-namespace DotNet8WebApi.EFCoreInfiniteScrollPagination.Api.Features
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(JsonConvert.SerializeObject(obj), "application/json");
-        }
+        return Content(JsonConvert.SerializeObject(obj), "application/json");
+    }
 
-        protected IActionResult InternalServerError(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+    protected IActionResult InternalServerError(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
     }
 }
--- DotNet8WebApi.EFCoreInfiniteScrollPagination.Api/GlobalUsings.cs
+++ DotNet8WebApi.EFCoreInfiniteScrollPagination.Api/GlobalUsings.cs
@@ -5,3 +5,4 @@ global using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features.Blog;
 global using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Resources;
 global using DotNet8WebApi.EFCoreInfiniteScrollPagination.Repositories.Features.Blog;
 global using Microsoft.AspNetCore.Mvc;
+global using Newtonsoft.Json;
