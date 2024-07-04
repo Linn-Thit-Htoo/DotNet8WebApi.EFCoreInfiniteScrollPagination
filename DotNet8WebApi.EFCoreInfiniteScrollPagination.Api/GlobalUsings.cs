dca819355fdb37974ec35ce2ee7dc264aa1df70d
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 20:26:28 +0630

    [Added] Global Usings to Api

---
+++ DotNet8WebApi.EFCoreInfiniteScrollPagination.Api/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features;
+global using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Features.Blog;
+global using DotNet8WebApi.EFCoreInfiniteScrollPagination.Models.Resources;
+global using DotNet8WebApi.EFCoreInfiniteScrollPagination.Repositories.Features.Blog;
